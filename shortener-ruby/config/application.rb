d43a83b8120aa69eb52e4fda4cf453ddb0afbc8c
obsessedyouth <52631736+obsessedyouth@users.noreply.github.com>
2021-01-08 00:51:58 +0100

    chore: add graphiql and db config

--- shortener-ruby/config/application.rb
+++ shortener-ruby/config/application.rb
@@ -5,14 +5,14 @@ require "rails"
 require "active_model/railtie"
 require "active_job/railtie"
 require "active_record/railtie"
-require "active_storage/engine"
+# require "active_storage/engine"
 require "action_controller/railtie"
 # require "action_mailer/railtie"
 # require "action_mailbox/engine"
-require "action_text/engine"
+# require "action_text/engine"
 require "action_view/railtie"
-require "action_cable/engine"
-# require "sprockets/railtie"
+# require "action_cable/engine"
+require "sprockets/railtie"
 require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
@@ -32,9 +32,7 @@ module ShortenerRuby
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
-    # Only loads a smaller set of middleware suitable for API only apps.
-    # Middleware like session, flash, cookies can be added back manually.
-    # Skip views, helpers and assets when generating a new resource.
-    config.api_only = true
+    # Don't generate system test files.
+    config.generators.system_tests = nil
   end
 end
